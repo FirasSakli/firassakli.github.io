b8cd4c719142f1096d890774f56fa5ffee8ad4e7
SaklyFiras <79334170+SaklyFiras@users.noreply.github.com>
2026-03-10 21:57:54 +0100

    modif : about me

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "REPO_NAME"; // replace with your actual repo name
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
