db258acbcd103e064654463c9c3c1a02b815af64
SaklyFiras <79334170+SaklyFiras@users.noreply.github.com>
2026-03-10 21:53:32 +0100

    modif : about me

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "REPO_NAME"; // replace with your actual repo name
+
 const nextConfig = {
-	output: "export",
-	images: {
-		unoptimized: true,
-	},
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
 };
 
 module.exports = nextConfig;
